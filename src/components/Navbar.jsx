3c989701906e7e8a554f0fe0663f7036a3a50632
Dipika <maharjandipika49@gmail.com>
2025-07-03 12:56:03 +0545

    hamburger icon added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,29 @@
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
 import '../styles/Navbar.css';
 
 export default function Navbar(){
-    return(
-        <nav className='navbar'>
-            <ul className='navbar-list'>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/projects">Projects</Link></li>
-                <li><Link to="/skills">Skills</Link></li>
-                <li><Link to="/contact">Contact</Link></li>
-            </ul>
+    const [isOpen, setIsOpen] = useState(false); //state variable to manage open/close state
 
-        </nav>
+    const toggleMenu = () => {
+        setIsOpen(!isOpen); //function to toggle menu state
+    }
+
+    return(
+        <div id='hero'>
+            <nav className='navbar' role='navigation'>
+                <button className='hamburger' onClick={toggleMenu} aria-label='Toggle menu' aria-expanded={isOpen}>   {/* aria is a special attributes you add to HTML to help people with disabilities use web content better */}
+                    <span className='bar'></span>   {/*hamburger icon for mobile view */}
+                    <span className='bar'></span>
+                    <span className='bar'></span>
+                </button>
+                <ul className={`navbar-list ${isOpen ? 'active' : ''}`}>   {/* Navigation menu list, 'active' class added if menu open */}
+                    <li><a href="#hero" onClick={() => setIsOpen(false)}>Home</a></li>
+                    <li><a href="#about" onClick={() => setIsOpen(false)}>About</a></li>
+                    <li><a href="#projects" onClick={() => setIsOpen(false)}>Projects</a></li>
+                    <li><a href="#skills" onClick={() => setIsOpen(false)}>Skills</a></li>
+                    <li><a href="#contact" onClick={() => setIsOpen(false)}>Contact</a></li>
+                </ul>
+            </nav>
+        </div>
     )
 }
