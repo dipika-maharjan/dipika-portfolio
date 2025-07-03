048cc966fc73dd62867fd6f5f091c0cc9710536a
Dipika <maharjandipika49@gmail.com>
2025-07-03 14:39:24 +0545

    projects updated

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,7 +8,7 @@ import bmiImage from '../assets/images/bmi.png';
 
 export default function Projects(){
     return(
-        <section className="projects-container">
+        <section id='projects' className="projects-container">
             <div className="projects-content">
                 <h3><i class="fa-solid fa-file-code"></i>Projects I've Built</h3>
                 <p>Here’s a glimpse into what I’ve been building — from fun mini tools to frontend projects and user-first designs.</p>
@@ -19,10 +19,13 @@ export default function Projects(){
                             <h4>Weather App</h4>
                             <img src = {weatherImage} alt = "weather img"/>
                             <p>A responsive weather dashboard built using React. 
-                                Fetches real-time weather data via OpenWeather API.</p>
-                                <br></br>
-                                <p><strong>Tools Used:</strong> React, API</p>
-                            <button>View Project</button>
+                                Fetches real-time weather data via OpenWeather API.
+                            </p>
+                            <br></br>
+                            <p><strong>Tools Used:</strong> React, API</p>
+                            <a href="https://github.com/dipika-maharjan/Weather-App" target="_blank" rel="noopener noreferrer">
+                                <button>View Project</button>
+                            </a>
                         </div>
                     </div>
 
@@ -33,7 +36,13 @@ export default function Projects(){
                             <p>A concept app where users can explore rustic living room designs for home inspiration. </p>
                             <br></br>
                             <p><strong>Tools Used:</strong> PostgreSQL, Express, React, Node.js</p>
-                            <button>View Project</button>
+                            <a href="https://github.com/dipika-maharjan/web-frontend" target="_blank" rel="noopener noreferrer">
+                                <button>Frontend Repo</button>
+                            </a>
+
+                            <a href="https://github.com/dipika-maharjan/web-backend" target="_blank" rel="noopener noreferrer" style={{marginLeft: '1rem'}}>
+                                <button>Backend Repo</button>
+                            </a>
                         </div>
                     </div>
 
@@ -44,7 +53,9 @@ export default function Projects(){
                             <p>A fun, interactive currency converter mini project to master core JavaScript logic and DOM manipulation.</p>
                             <br></br>
                             <p><strong>Tools Used:</strong> HTML, CSS, JavaScript</p>
-                            <button>View Project</button>
+                            <a href="https://github.com/dipika-maharjan/Currency-Converter" target="_blank" rel="noopener noreferrer">
+                                <button>View Project</button>
+                            </a>
                         </div>
                     </div>
 
@@ -55,7 +66,9 @@ export default function Projects(){
                             <p>A fully responsive clone of the Amazon homepage layout, built with pure HTML and CSS. </p>
                             <br></br>
                             <p><strong>Tools Used:</strong> HTML, CSS</p>
-                            <button>View Project</button>
+                            <a href="https://github.com/dipika-maharjan/Amazon-Clone-Project" target="_blank" rel="noopener noreferrer">
+                                <button>View Project</button>
+                            </a>
                         </div>
                     </div>
 
@@ -66,7 +79,9 @@ export default function Projects(){
                             <p>A mobile app prototype designed to promote mindful self-care called YouMatter.</p>
                             <br></br>
                             <p><strong>Tools Used:</strong> Figma, UX Research, Prototyping</p>
-                            <button>View Project</button>
+                            <a href="https://www.figma.com/proto/1rhWS3uH0wiizhpqjYwILs/Mental-Health-App?node-id=46-12&starting-point-node-id=46%3A12&t=PhVdC0zi5Z8OBYHM-1https://www.figma.com/proto/1rhWS3uH0wiizhpqjYwILs/Mental-Health-App?node-id=46-12&starting-point-node-id=46%3A12&t=PhVdC0zi5Z8OBYHM-1" target="_blank" rel="noopener noreferrer">
+                                <button>View Project</button>
+                            </a>
                         </div>
                     </div>
 
@@ -74,10 +89,12 @@ export default function Projects(){
                         <div className='project-details'>
                             <h4>BMI Calculator App</h4>
                             <img src = {bmiImage} alt = "bmi img"/>
-                            <p>Currently building a BMI and health plan generator using React for frontend and Flask (Python) for backend. </p>
+                            <p>Currently building a BMI and health plan generator group project using React for frontend and Flask (Python) for backend. </p>
                             <br></br>
                             <p><strong>Tools Used:</strong> React, Python Flask, Axios</p>
-                            <button>In progress</button>
+                            <a href="https://github.com/iamsabinakhanal/Smart-BMI-calculator" target="_blank" rel="noopener noreferrer">
+                                <button>In Progress</button>
+                            </a>
                         </div>
                     </div>
                 </div>
